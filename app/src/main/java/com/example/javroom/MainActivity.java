11aef3f3a5e44475d837db67298c07587524d0ae
amelh <kloyn321@gmail.com>
2023-03-29 02:40:13 +0300

    ViewModelProvider

--- app/src/main/java/com/example/javroom/MainActivity.java
+++ app/src/main/java/com/example/javroom/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.javroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this)
+                .get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onChange", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
