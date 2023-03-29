2c6bd30c95f51c33e226bfa1b81a83d78a18c4f2
amelh <kloyn321@gmail.com>
2023-03-29 03:16:32 +0300

    add recycler LayoutManager

--- app/src/main/java/com/example/javroom/MainActivity.java
+++ app/src/main/java/com/example/javroom/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider(this)
                 .get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-                Toast.makeText(MainActivity.this, "onChange", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
